f03ec8e7ad573e50c8c622b54f510514e4debb69
Rupesh Baghmar <rupesh@housejoy.in>
2020-05-10 18:40:18 +0530

    Updates

---
+++ precache-manifest.d0bdf43bee7d1e0fc6bed77f1ba3e2cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff73bcbe7da7694097d0d59da9077ce",
+    "url": "/react-restro/index.html"
+  },
+  {
+    "revision": "486396281e1f96916436",
+    "url": "/react-restro/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9a246c207224aba96f70",
+    "url": "/react-restro/static/css/main.bad9ce8c.chunk.css"
+  },
+  {
+    "revision": "486396281e1f96916436",
+    "url": "/react-restro/static/js/2.9847903c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-restro/static/js/2.9847903c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a246c207224aba96f70",
+    "url": "/react-restro/static/js/main.0e60a7d9.chunk.js"
+  },
+  {
+    "revision": "b08af231ef24bf72087a",
+    "url": "/react-restro/static/js/runtime-main.bf952fea.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69cdae262d1eaf124cb4907bf0106101.js"
+  "/react-restro/precache-manifest.d0bdf43bee7d1e0fc6bed77f1ba3e2cc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-restro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
