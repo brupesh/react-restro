39c93aba217baa550c017ba2add24041161713f2
Rupesh Baghmar <rupesh@housejoy.in>
2020-05-10 17:24:53 +0530

    first commit

--- src/http-common.js
+++ src/http-common.js
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-const UNAUTHORIZED = 401;
-axios.interceptors.response.use(
-  response => response,
-  error => {
-    const {status} = error.response;
-    if (status === UNAUTHORIZED) {
-      dispatch(userSignOut());
-    }
-    return Promise.reject(error);
- }
-);
-
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
